a90123cf333a4a96fc78b3e78a996a0eaea8458d
Rich Caloggero <rjc@mit.edu>
2025-12-18 19:31:24 -0500

    ** trying to create same code for component and for non-component
    ** not liking the "file:" restriction; making it difficult to test for colleagues without a dev server

--- imageGallery.js
+++ imageGallery.js
@@ -1,27 +1,90 @@
-import { imageData } from "./imageData.js";
+let connectionCount = 0;
+function genPopoverId () {return `imageGallery-image-display-${connectionCount}`;}
+function not (x) {return !x;}
 
-document.body.insertAdjacentHTML("beforeEnd", `
-<dialog popover id="image-display">
-<div><button autofocus popoverTarget="image-display" popoverAction="hide">Close</button></div>
+function connect (element,document, cssText) {
+const popoverId = genPopoverId();
+connectionCount += 1;
+
+let thumbnails, imageDisplay, style;
+
+if (cssText) {
+(style = document.createElement("style"))
+.textContent = cssText;
+element.insertAdjacentElement("afterBegin", style);
+} // if
+
+(thumbnails = document.createElement("ul", popoverId));
+thumbnails.className = "thumbnails";
+element.appendChild(thumbnails);
+
+(imageDisplay = document.createElement("dialog"))
+.insertAdjacentHTML("beforeEnd", `
+<div><button autofocus popoverTarget="${popoverId}" popoverAction="hide">Close</button></div>
 <div class="image" aria-hidden="true"><img src="" alt=""></div>
 <div class="long-description"></div>
 `);
+imageDisplay.id = popoverId;
+imageDisplay.popover = "auto";
+imageDisplay.className = "image-display";
+element.appendChild(imageDisplay);
+imageDisplay.addEventListener("beforetoggle", updateImageDisplay);
 
-createThumbnails (imageData, document.querySelector(".image-gallery"));
-document.querySelector("#image-display").addEventListener("beforetoggle", updateImageDisplay);
+if (element.dataset.src) {
+loadImageData (element.dataset.src, thumbnails, popoverId);
+}else {
+wrapThumbnails([...element.children].filter(element => element.matches("img")), thumbnails, popoverId);
+} // if
+} // connect
 
-function createThumbnails (imageData, gallery) {
+async function loadImageData (src, thumbnails, id) {
+try {
+const imageData = await import(src, {with: {type: "json"}});
+createThumbnails (imageData.default, thumbnails, id);
+return;
+
+} catch (e) {
+console.log(`outer catch: ${e}`);
+const imageData = await fetch(src);
+try {
+console.log(`inner catch: ${e}`);
+debugger;
+
+} catch (e) {
+alert (`cannot parse json: ${e}`);
+throw (e);
+} // try
+} // try
+} // async loadImageData
+
+
+function createThumbnails (imageData, gallery, id) {
 for (const data of imageData) {
 gallery.insertAdjacentHTML("beforeEnd", `
-<li><button popoverTarget="image-display" popoverAction="show">
+<li><button popoverTarget="${id}" popoverAction="show">
 <img
-alt="${data.alt || "decorative"}"
-src="${data.url? data.url : data/path}"
+alt="${data.alt.trim() || "decorative"}"
+src="${data.url.trim()? data.url : data.path}"
 data-description="${data.description}"
 ></button></li>
 `);
 } // for
-} // importImageData
+} // createThumbnails
+
+function wrapThumbnails (images, thumbnails, id) {
+for (const img of images) {
+const li = document.createElement("li");
+const button = document.createElement("button");
+button.setAttribute("popovertarget",  id);
+button.setAttribute("popoveraction", "show");
+
+thumbnails.appendChild(li);
+li.appendChild(button);
+//img.replaceWith(li);
+button.appendChild(img);
+} // for
+
+} // wrapThumbnails
 
 function updateImageDisplay (e) {
 console.log("before popup ", e.target, e);
@@ -33,3 +96,4 @@ const image = e.target.querySelector(".image img");
 image.src="thumbnail.src";
 longDescription.innerHTML = descriptionText;
 } // updateImageDisplay
+
--- imageGallery.wc.js
+++ imageGallery.wc.js
@@ -1,3 +1,5 @@
+//import { connect } from "./imageGallery.js";
+
 /* Usage:
 <image-gallery data-images="./someFile.json"></image-gallery>
 <image-gallery format="inline || block || ordered || unordered" width="thumbnail width" height="thumbnail height">
@@ -7,7 +9,6 @@
 */
 
 const cssText = `@scope {
-
 .thumbnails { /* style the list of thumbnails (there are properties you can use to make it look like a grid) */
 list-style: none;
 column-count: 3;
@@ -43,47 +44,14 @@ dialog:open { /* style dialog when it is visible */
 
 
 
-let dialogCount = 0;
 class ImageGallery extends HTMLElement {
-#popoverId = `image-gallery-image-display-${++dialogCount}`;
 
 constructor () {
 super();
 } // constructor
 
 connectedCallback () {
-let thumbnails, imageDisplay, style;
-
-(style = document.createElement("style"))
-.textContent = cssText;
-this.insertAdjacentElement("afterBegin", style);
-
-(thumbnails = document.createElement("ul"));
-thumbnails.className = "thumbnails";
-this.appendChild(thumbnails);
-
-(imageDisplay = document.createElement("dialog"))
-.insertAdjacentHTML("beforeEnd", `
-<div><button autofocus popoverTarget="image-display" popoverAction="hide">Close</button></div>
-<div class="image" aria-hidden="true"><img src="" alt=""></div>
-<div class="long-description"></div>
-`);
-imageDisplay.id = this.#popoverId;
-imageDisplay.popover = "auto";
-imageDisplay.className = "image-display";
-this.appendChild(imageDisplay);
-imageDisplay.addEventListener("beforetoggle", updateImageDisplay);
-
-if (this.dataset.images) {
-import(this.dataset.images, {with: {type: "json"}})
-.then (imageData => {
-createThumbnails (imageData.default, thumbnails, this.#popoverId)
-});
-
-}else {
-wrapThumbnails([...this.children].filter(element => element.matches("img")), thumbnails, this.#popoverId);
-} // if
-
+connect(this,document, cssText);
 } // connectedCallback
 
   connectedMoveCallback() {
@@ -93,41 +61,3 @@ wrapThumbnails([...this.children].filter(element => element.matches("img")), thu
 
 customElements.define("image-gallery", ImageGallery);
 
-function createThumbnails (imageData, gallery, id) {
-for (const data of imageData) {
-gallery.insertAdjacentHTML("beforeEnd", `
-<li><button popoverTarget="${id}" popoverAction="show">
-<img
-alt="${data.alt.trim() || "decorative"}"
-src="${data.url.trim()? data.url : data.path}"
-data-description="${data.description}"
-></button></li>
-`);
-} // for
-} // createThumbnails
-
-function wrapThumbnails (images, thumbnails, id) {
-for (const img of images) {
-const li = document.createElement("li");
-const button = document.createElement("button");
-button.setAttribute("popovertarget",  id);
-button.setAttribute("popoveraction", "show");
-
-thumbnails.appendChild(li);
-li.appendChild(button);
-//img.replaceWith(li);
-button.appendChild(img);
-} // for
-
-} // wrapThumbnails
-
-function updateImageDisplay (e) {
-console.log("before popup ", e.target, e);
-const thumbnail = e.source.querySelector("img");
-const descriptionText = thumbnail.dataset.description;
-const longDescription = e.target.querySelector(".long-description");
-const image = e.target.querySelector(".image img");
-
-image.src="thumbnail.src";
-longDescription.innerHTML = descriptionText;
-} // updateImageDisplay
